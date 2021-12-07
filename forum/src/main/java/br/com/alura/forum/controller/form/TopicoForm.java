06d6d6ceb3d0ed545186b914131a241a1e7a71dd
Michel Junior <jrcassunde95@gmail.com>
2021-12-07 13:42:02 -0300

    implementação metodos de delete e atualizar

--- forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,9 +1,10 @@
 package br.com.alura.forum.controller.form;
 
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
@@ -11,11 +12,11 @@ import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
-	@NotNull @NotEmpty @Min(5)
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
-	@NotNull @NotEmpty @Min(5)
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
-	@NotNull @NotEmpty @Min(5)
+	@NotNull @NotEmpty @Length(min = 5)
 	private String nomeCurso;
 
 	public String getTitulo() {
