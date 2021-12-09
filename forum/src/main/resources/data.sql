f7f8c275867212e1fca28bd56d11ef6e25f09422
Michel Junior <jrcassunde95@gmail.com>
2021-12-09 09:44:00 -0300

    implementando spring security na API

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$rsJxUwIyYyCm9y9Yce4U7unjeV/dpvpZy3kaEnsjAhMIMyYtPL9n6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
